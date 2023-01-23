ca7dad843d88906770540b883a65894b4f4d3e24
ksivabalan91 <ksivabalan.91@gmail.com>
2023-01-23 21:22:26 +0800

    cleared up higlighted problems

--- src/sdf/FileIO.java
+++ src/sdf/FileIO.java
@@ -2,7 +2,6 @@ package sdf;
 
 import java.io.BufferedWriter;
 import java.io.File;
-import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
 
--- src/sdf/Main.java
+++ src/sdf/Main.java
@@ -2,29 +2,26 @@ package sdf;
 
 import java.io.File;
 import java.io.IOException;
-import java.util.LinkedList;
 import java.util.List;
 
 public class Main {
 
     public static void main(String[] args) throws IOException {
-        // ArraySortExample.example01();
-        // System.out.println("\n");
-        // ArraySortExample.example02();
-        // System.out.println("\n");
-        // CollectionSortExample.example01();
-        // System.out.println("\n");
-        // CollectionSortExample.example02();
-        // System.out.println("\n");
-        // HashMapExample.example01();
-        // System.out.println("\n");
-        // ConcurrentHashMapExample.example01();
-        // System.out.println("\n");
-        // LinkedListExample.example01();
-        // System.out.println("\n");
-        // StackExample.example01();
-
-   
+        ArraySortExample.example01();
+        System.out.println("\n");
+        ArraySortExample.example02();
+        System.out.println("\n");
+        CollectionSortExample.example01();
+        System.out.println("\n");
+        CollectionSortExample.example02();
+        System.out.println("\n");
+        HashMapExample.example01();
+        System.out.println("\n");
+        ConcurrentHashMapExample.example01();
+        System.out.println("\n");
+        LinkedListExample.example01();
+        System.out.println("\n");
+        StackExample.example01();
 
         File inputFile = new File("data\\employees.csv");
         List<String> listEmployees = csvReader.readFile(inputFile);
@@ -32,16 +29,8 @@ public class Main {
         System.out.println(listEmployees.toString());
 
         // csvWriter picasso = new csvWriter();
-
         // try{picasso.writeToCSV(listEmployees, "data\\employees.csv");
         // } catch(IOException e){}
-
-        
-
-
-
-
-        
         
     }
 }
